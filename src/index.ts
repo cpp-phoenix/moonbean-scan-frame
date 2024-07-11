5445411239d86674561629c48d04602f4e60870d
phoenix <phoenix@phoenixs-MacBook-Pro.local>
2024-07-11 18:02:21 +0530

    update: finish moonbeam scan

--- src/index.ts
+++ src/index.ts
@@ -5,22 +5,11 @@ import type { FrameSignaturePacket } from './types'
 
 const app = new Hono()
 
-const endpoint = 'https://api.subquery.network/sq/subquery/subquery-mainnet' // Replace with your actual URL
+const moonbeam_endpoint = 'https://api.subquery.network/sq/kumawallet/kuma-moonbeam'
+let FRAME_URL=""
 const DEFAULT_IMAGE = 'https://imageplaceholder.net/600x400/7b68ee/ffffff?text='
 
-interface Era {
-  id: number
-  createdBlock: number
-}
-
-function numberToHexByte(num: number): string {
-  if (num < 0 || num > 255) {
-    throw new Error('Number out of byte range (0-255)')
-  }
-  return `0x${num.toString(16).padStart(2, '0')}`
-}
-
-function returnHTML(imageLink: string) {
+function returnResponsePage(imageLink: string, inputText: string) {
   return html`
     <html lang="en">
       <head>
@@ -28,20 +17,36 @@ function returnHTML(imageLink: string) {
         <meta property="fc:frame" content="vNext" />
         <meta property="fc:frame:image" content="${imageLink}" />
         <meta property="fc:frame:image:aspect_ratio" content="1.91:1" />
-        <meta property="fc:frame:input:text" content="Check SubQuery Era" />
-        <meta property="fc:frame:button:1" content="Submit" />
+        <meta name="fc:frame:post_url" content="${FRAME_URL}/home?hash=${inputText}" />
+        <meta property="fc:frame:button:1" content="Go Back" />
+        <meta property="fc:frame:button:2" content="View on explorer" />
+        <meta name="fc:frame:button:2:action" content="post_redirect" />
+        <meta http-equiv="refresh" content="0; url=https://moonscan.io/tx/${inputText}" />
         <title>Farcaster Frames</title>
       </head>
-      <body>
-        <h1>Hello Farcaster!</h1>
-        <p className="mb-10">Refresh browser to refresh image</p>
-      </body>
+    </html>
+  `
+}
+
+function returnHomePage(imageLink: string) {
+  return html`
+    <html lang="en">
+      <head>
+        <meta property="og:image" content="${imageLink}" />
+        <meta property="fc:frame" content="vNext" />
+        <meta property="fc:frame:image" content="${imageLink}" />
+        <meta property="fc:frame:image:aspect_ratio" content="1.91:1" />
+        <meta property="fc:frame:input:text" content="Enter the txn hash" />
+        <meta property="fc:frame:button:1" content="Search" />
+        <meta http-equiv="refresh" content="0; url=https://moonscan.io/txs" />
+        <title>Farcaster Frames</title>
+      </head>
     </html>
   `
 }
 
 async function fetchGraphQL(query: string) {
-  const response = await fetch(endpoint, {
+  const response = await fetch(moonbeam_endpoint, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ query }),
@@ -56,75 +61,76 @@ async function fetchGraphQL(query: string) {
 }
 
 app.get('/', async (c) => {
-  try {
-    const query = `query {
-      eras (orderBy: ID_DESC first: 1) {
-        nodes {
-          id
-          startTime
-          endTime
-          createdBlock
-          lastEvent
-        }
-      }
-    }`
-    const data = await fetchGraphQL(query)
-    const node = data.data.eras.nodes[0]
+  console.log("in get /")
+  const frameImage = `${DEFAULT_IMAGE}Search+txns+on+Moonbeam+by+txn+hash`
+  return c.html(returnHomePage(frameImage))
+})
 
-    if (node) {
-      const era: Era = {
-        id: parseInt(node.id, 16),
-        createdBlock: node.createdBlock,
-      }
+app.post('/redirect', (c) => {
+  console.log("Reached here!!")
+  return c.redirect('https://moonscan.io/txs')
+})
 
-      const frameImage = `${DEFAULT_IMAGE}Current++SubQuery+Era:+${era.id}`
-      return c.html(returnHTML(frameImage))
-    } else {
-      const frameImage = `${DEFAULT_IMAGE}Invalid+response`
-      return c.html(returnHTML(frameImage))
-    }
-  } catch (error) {
-    console.error('Fetch error:', error)
-    const frameImage = `${DEFAULT_IMAGE}Error+while+fetching`
-    return c.html(returnHTML(frameImage))
-  }
+app.get('/ping', (c) => {
+  console.log("in ping")
+  return c.text(FRAME_URL)
+})
+
+app.get('/seturl', (c) => {
+  console.log("in /seturl")
+  const _frameUrl = c.req.query('url')
+  if(_frameUrl !== undefined)
+    FRAME_URL = _frameUrl
+  return c.text('success')
 })
 
 app.post('/', async (c) => {
+  console.log("in /")
   try {
     const body = await c.req.json<FrameSignaturePacket>()
     const { inputText } = body.untrustedData
 
-    if (!Number.isInteger(Number(inputText))) {
-      const frameImage = `${DEFAULT_IMAGE}Invalid+value+entered`
-      return c.html(returnHTML(frameImage))
-    }
+    console.log("Input is: " + inputText)
 
     const query = `query {
-      era(id: "${numberToHexByte(Number(inputText))}") {
-        startTime
-        nodeId
-        lastEvent
-        id
-        endTime
-        createdBlock
+      transaction(id: "${inputText}") {
+        blockNumber
+        status
       }
     }`
 
     const data = await fetchGraphQL(query)
-    const node = data.data.era
+    const txnData = data.data.transaction
 
-    if (!node) {
-      const frameImage = `${DEFAULT_IMAGE}Era+with+specified+id+was+not+found`
-      return c.html(returnHTML(frameImage))
+    if (!txnData) {
+      const frameImage = `${DEFAULT_IMAGE}txn+with+specified+hash+was+not+found`
+      return c.html(returnResponsePage(frameImage, inputText))
     }
 
-    const frameImage = `${DEFAULT_IMAGE}Era+${Number(inputText)}+started+on+block+${node.createdBlock}`
-    return c.html(returnHTML(frameImage))
+    const frameImage = `${DEFAULT_IMAGE}Txn+is+included+in+block: ${txnData["blockNumber"]} with+status: ${txnData["status"].toUpperCase()}`
+    return c.html(returnResponsePage(frameImage, inputText))
   } catch (error) {
     console.error('Fetch error:', error)
-    const frameImage = `${DEFAULT_IMAGE}Error+during+Era+data+fetching`
-    return c.html(returnHTML(frameImage))
+    const frameImage = `${DEFAULT_IMAGE}Error+during+moonbeam+data+fetching`
+    return c.html(returnResponsePage(frameImage, ""))
+  }
+})
+
+app.post('/home', async (c) => {
+  console.log("in home")
+
+  const body = await c.req.json<FrameSignaturePacket>()
+  const buttonIndex = body.untrustedData.buttonIndex
+
+  const hash = c.req.query('hash')
+
+  console.log(buttonIndex)
+
+  if(buttonIndex == 1) {
+    const frameImage = `${DEFAULT_IMAGE}Search+txns+on+Moonbeam+by+txn+hash`
+    return c.html(returnHomePage(frameImage))
+  } else {
+    return c.redirect(`https://moonscan.io/tx/${hash}`)
   }
 })
 
